9039473d7c9e8380d854021fc2a80b0dbd50e040
c-wei <caroline5wei@gmail.com>
2025-08-07 00:14:57 -0400

    v1

--- lab3/hash-table-v1.c
+++ lab3/hash-table-v1.c
@@ -21,11 +21,16 @@ struct hash_table_entry {
 
 struct hash_table_v1 {
 	struct hash_table_entry entries[HASH_TABLE_CAPACITY];
+	pthread_mutex_t lock;
 };
 
 struct hash_table_v1 *hash_table_v1_create()
 {
 	struct hash_table_v1 *hash_table = calloc(1, sizeof(struct hash_table_v1));
+
+	uint32_t err = pthread_mutex_init(&hash_table->lock, NULL);
+	if (err != 0) exit(err);
+
 	assert(hash_table != NULL);
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
@@ -72,6 +77,9 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
                              const char *key,
                              uint32_t value)
 {
+	uint32_t err = pthread_mutex_lock(&hash_table->lock);
+	if(err != 0) exit(err);
+
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -79,6 +87,8 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	/* Update the value if it already exists */
 	if (list_entry != NULL) {
 		list_entry->value = value;
+		err = pthread_mutex_unlock(&hash_table->lock);
+		if(err != 0) exit(err);
 		return;
 	}
 
@@ -86,6 +96,9 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+
+	err = pthread_mutex_unlock(&hash_table->lock);
+	if(err != 0) exit(err);
 }
 
 uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
@@ -110,5 +123,7 @@ void hash_table_v1_destroy(struct hash_table_v1 *hash_table)
 			free(list_entry);
 		}
 	}
+	uint32_t err = pthread_mutex_destroy(&hash_table->lock);
+	if(err != 0) exit(err);
 	free(hash_table);
 }
